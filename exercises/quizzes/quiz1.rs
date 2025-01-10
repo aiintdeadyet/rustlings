90072de9234b76331d1c8183aa1fa85cb5d9c595
aiin'tdeadyet <jamesdcritchlow@gmail.com>
2025-01-09 22:02:24 -0700

    Finnished quiz for 00 - 04

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,6 +12,15 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+// use std::i32;
+
+fn calculate_price_of_apples(num:i32) -> i32 {
+    if num > 40 {
+        return num;
+    }
+    num * 2 // return num * 2
+}
+
 fn main() {
     // You can optionally experiment here.
 }
