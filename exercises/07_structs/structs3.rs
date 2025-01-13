f8066e5376616327ac928d70013303fc2db04b4e
aiin'tdeadyet <jamesdcritchlow@gmail.com>
2025-01-13 16:00:40 -0700

    finnished 07_structs

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -1,6 +1,8 @@
 // Structs contain data, but can also have logic. In this exercise, we have
 // defined the `Package` struct, and we want to test some logic attached to it.
 
+// use std::u32;
+
 #[derive(Debug)]
 struct Package {
     sender_country: String,
@@ -24,14 +26,19 @@ impl Package {
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn is_international(&self) {
+    fn is_international(&self) -> bool {
         // TODO: Read the tests that use this method to find out when a package
         // is considered international.
+        if self.sender_country == self.recipient_country {
+            return false;
+        }
+        true
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn get_fees(&self, cents_per_gram: u32) {
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
         // TODO: Calculate the package's fees.
+        cents_per_gram * self.weight_in_grams
     }
 }
 
