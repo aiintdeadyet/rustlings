6f3fb7d387601489f352585c9497f7d135591f71
aiin'tdeadyet <jamesdcritchlow@gmail.com>
2025-01-15 11:34:31 -0700

    finnished 09_strings

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,38 @@
+
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    // let mut first: usize = 0;
+    // for (i, c) in input.char_indices() {
+    //     first = i;
+    //     if c != ' ' {
+    //         break;
+    //     }
+    // }    
+    // let mut last: usize = 0;
+    // for (i, c) in input.char_indices() {
+    //     last = i;
+    //     if c != ' ' {
+    //         break;
+    //     }
+    // }
+    // output = input
+    // input
+    let chars_to_trim: &[char] = &[' '];
+    let output: &str = input.trim_matches(chars_to_trim);
+    output
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let mut output = String::from(input);
+    output.push_str(" world!");
+    output
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    let output = String::from(input);
+    output.replace("cars", "balloons")
 }
 
 fn main() {
